6d50ab6360bd203917ab25df6074b6147b4346c1
YOONJIHAE <jihae.yoon@musinsa.com>
2022-10-11 22:39:08 +0900

    [lec08] 코틀린에서 함수를 다루는 방법

---
+++ src/main/kotlin/org/example/lec08/Lec08Main.kt
@@ -0,0 +1,36 @@
+package org.example.lec08
+
+fun main() {
+    repeat("hello world1", 5, true)
+    repeat("hello world2")
+
+    printNameAndGender(name = "jihae", gender = "F")
+
+    printAll("A", "B", "C")
+
+    val array = arrayOf("A", "B", "C")
+    printAll(*array)
+}
+
+fun max(a: Int, b: Int) = if (a > b) a else b
+
+fun repeat(str: String, num: Int = 3, useNewLine: Boolean = true) {
+    for (i in 1..num) {
+        if (useNewLine) {
+            println(str)
+        } else {
+            print(str)
+        }
+    }
+}
+
+fun printNameAndGender(name: String, gender: String) {
+    println(name)
+    println(gender)
+}
+
+fun printAll(vararg strings: String) {
+    for (str in strings) {
+        println(str)
+    }
+}
