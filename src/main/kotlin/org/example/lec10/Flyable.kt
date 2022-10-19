bf43cf22e52a0a4e6d33fe3b511aa1b28e324976
YOONJIHAE <jihae.yoon@musinsa.com>
2022-10-19 15:14:11 +0900

    [lec10] 코틀린에서 상속을 다루는 방법

---
+++ src/main/kotlin/org/example/lec10/Flyable.kt
@@ -0,0 +1,10 @@
+package org.example.lec10
+
+interface Flyable {
+    fun act() {
+        println("파닥파닥")
+    }
+
+//    fun fly()
+
+}
---
+++ src/main/kotlin/org/example/lec10/Swimable.kt
@@ -0,0 +1,12 @@
+package org.example.lec10
+
+interface Swimable {
+
+    val swimAbility: Int
+        get() = 3
+
+    fun act() {
+        println(swimAbility)
+        println("어푸 어푸")
+    }
+}
